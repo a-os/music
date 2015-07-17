c7b0187f553bf7b218b3f5c3838f5f02432f032e
Alive Kuo <alegnadise@gmail.com>
2015-07-17 16:08:09 +0800

    Fix shared path in build

--- build/build.js
+++ build/build.js
@@ -7,7 +7,7 @@ exports.execute = function(options) {
   utils.copyToStage(options);
   var sharedPath = utils.gaia.getInstance(options).sharedFolder.path;
   var paths = [
-    [sharedPath, 'js', 'blobview.js'],
+    [sharedPath, 'blobview', 'blobview.js'],
     [options.APP_DIR, 'js', 'metadata', 'formats.js'],
     [options.APP_DIR, 'js', 'metadata', 'core.js']
   ];
